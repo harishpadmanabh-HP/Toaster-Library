1a77c0ca0e91b2d75cd1607631bb1e57a19df248
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2019-06-30 00:19:34 +0530

    fff

--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -1,6 +1,8 @@
 package com.example.toasterlibrary;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 public class ToasterMessage {
@@ -10,5 +12,30 @@ public class ToasterMessage {
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
     }
+
+    public static void showExitDiaolog(final Context c)
+        {
+
+            AlertDialog.Builder builder;
+            builder = new AlertDialog.Builder(c);
+
+            builder.setMessage("Do you want to close this application ?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Toast.makeText(c,"you choose yes action for alertbox",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //  Action for 'NO' Button
+                            dialog.cancel();
+                            Toast.makeText(c,"you choose no action for alertbox",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }
 
 }
