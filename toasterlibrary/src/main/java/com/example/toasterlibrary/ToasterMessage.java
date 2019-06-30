89b48a413d157299550be7d845011e6166c37236
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2019-06-30 11:59:26 +0530

    warning toast error,info,custom toast

--- toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -4,11 +4,14 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Typeface;
+import android.view.Gravity;
 import android.widget.Toast;
 import com.mrntlu.toastie.*;
 
 
 public class ToasterMessage {
+
+    int icon=R.drawable.ic_check_circle_black_24dp;
 
 
     public static void toaster(Context c, String message){
@@ -19,6 +22,7 @@ public class ToasterMessage {
 
 
 
+    //warning toast
     public static void warningToaster(Context c, String message,String postionOfToast) {
 
 
@@ -44,8 +48,131 @@ public class ToasterMessage {
 
         }
 
+    }
+    //success toast
+    public static void successToaster(Context c, String message,String postionOfToast) {
+
+
+        if(postionOfToast.equalsIgnoreCase("bottom"))
+        {
+            Toastie.success(c,message, Toast.LENGTH_LONG).show();
+
+        }
+        else if(postionOfToast.equalsIgnoreCase("centre"))
+        {
+            Toastie.centerSuccess(c,message, Toast.LENGTH_LONG).show();
+
+        }
+        else if(postionOfToast.equalsIgnoreCase("top"))
+        {
+            Toastie.topSuccess(c,message, Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toastie.success(c,message, Toast.LENGTH_LONG).show();
+
+        }
 
 
     }
 
+    //info toast
+
+    public static void infoToaster(Context c, String message,String postionOfToast) {
+
+        if(postionOfToast.equalsIgnoreCase("bottom"))
+        {
+            Toastie.info(c,message, Toast.LENGTH_LONG).show();
+
+        }
+        else if(postionOfToast.equalsIgnoreCase("centre"))
+        {
+            Toastie.centerInfo(c,message, Toast.LENGTH_LONG).show();
+
+        }
+        else if(postionOfToast.equalsIgnoreCase("top"))
+        {
+            Toastie.topInfo(c,message, Toast.LENGTH_LONG).show();
+
+        }
+        else
+        {
+            Toastie.info(c,message, Toast.LENGTH_LONG).show();
+
+        }
+
+
     }
+
+
+
+        //custom toast
+
+    public static void customToaster(Context c, String message,String postionOfToast,int icon,int cardBackcolor,int textSize,int cardRadious) {
+
+        if(postionOfToast.equalsIgnoreCase("bottom"))
+        {
+            Toastie.allCustom(c)
+                    .setTypeFace(Typeface.DEFAULT_BOLD)
+                    .setTextSize(textSize)
+                    .setCardRadius(cardRadious)
+                    .setCardElevation(10)
+                    .setIcon(icon)
+                    .setCardBackgroundColor(cardBackcolor)
+                    .setMessage("Fully customizable toast. But in a different way.")
+                    .setGravity(Gravity.BOTTOM,5,5)
+                    .createToast(Toast.LENGTH_LONG)
+                    .show();
+        }
+        else if(postionOfToast.equalsIgnoreCase("centre"))
+        {
+            Toastie.allCustom(c)
+                    .setTypeFace(Typeface.DEFAULT_BOLD)
+                    .setTextSize(textSize)
+                    .setCardRadius(cardRadious)
+                    .setCardElevation(10)
+                    .setIcon(icon)
+                    .setCardBackgroundColor(cardBackcolor)
+                    .setMessage("Fully customizable toast. But in a different way.")
+                    .setGravity(Gravity.CENTER,5,5)
+                    .createToast(Toast.LENGTH_LONG)
+                    .show();
+        }
+        else if(postionOfToast.equalsIgnoreCase("top"))
+        {
+            Toastie.allCustom(c)
+                    .setTypeFace(Typeface.DEFAULT_BOLD)
+                    .setTextSize(textSize)
+                    .setCardRadius(cardRadious)
+                    .setCardElevation(10)
+                    .setIcon(icon)
+                    .setCardBackgroundColor(cardBackcolor)
+                    .setMessage("Fully customizable toast. But in a different way.")
+                    .setGravity(Gravity.TOP,5,5)
+                    .createToast(Toast.LENGTH_LONG)
+                    .show();
+        }
+        else
+        {
+            Toastie.allCustom(c)
+                    .setTypeFace(Typeface.DEFAULT_BOLD)
+                    .setTextSize(textSize)
+                    .setCardRadius(cardRadious)
+                    .setCardElevation(10)
+                    .setIcon(icon)
+                    .setCardBackgroundColor(cardBackcolor)
+                    .setMessage("Fully customizable toast. But in a different way.")
+                    .setGravity(Gravity.BOTTOM,5,5)
+                    .createToast(Toast.LENGTH_LONG)
+                    .show();
+        }
+
+
+
+
+    }
+
+
+
+}
